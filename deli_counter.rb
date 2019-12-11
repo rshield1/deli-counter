318b93cc5c3c0f03effb02b0872e6b79b26c4a7c
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 22:43:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli != []
+    puts katz_deli
+  else
+    puts "The line is currently empty"
+  
+
+    
+  
+end
+
+
+
+def take_a_number(katz_deli, name)
+  
+  katz_deli.push(name)
+    puts Welcome, #{name}. You are number #{num} in line
+  
+  
+end
+
+
+
+def now_serving(katz_deli)
+  puts the next person in line then remove them from the front
+  puts "Currently serving #{name}."
+else
+  if nobody in line, puts "There is nobody waiting to be served!"
+end
+
+
+  #Review adding and removing elements from an array as well as iterating with index numbers. Also, many of the methods to add and remove elements from an array can also be used to add and remove elements to a string. This will help you solve the lab.
